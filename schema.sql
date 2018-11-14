c400d39bc46d43d2569dc19f185f1f1265588dd9
Martijn <email@martijnbrekelmans.com>
2018-11-15 00:24:02 +0100

    Add proposed normalization schema for the database

--- schema.sql
+++ schema.sql
@@ -1,5 +1,23 @@
+PRAGMA foreign_keys = ON;
+
 CREATE TABLE function_calls
-(name text, params text, returnval text, filename text, linenum integer);
+(
+    `name` TEXT,
+    `params` TEXT,
+    `returnval` TEXT,
+    `calling_filename` INTEGER,
+    `definition_filename` INTEGER,
+    `linenum` INTEGER,
+
+    FOREIGN KEY(`calling_filename`) REFERENCES `file_names`(`rowid`),
+    FOREIGN KEY(`definition_filename`) REFERENCES `file_names`(`rowid`)
+);
 
 CREATE TABLE traces
-(id int, `timestamp` text);
+(`requestname` TEXT, `timestamp` TEXT);
+
+CREATE TABLE file_names
+(`name` TEXT);
+
+CREATE TABLE function_names
+(`name` TEXT);
