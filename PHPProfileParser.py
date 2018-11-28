a481567b622860c5373dcc432748c0736844b1e8
Martijn <email@martijnbrekelmans.com>
2018-11-28 01:12:14 +0100

    Performance: Optimize profile parser ~30% speedup

--- PHPProfileParser.py
+++ PHPProfileParser.py
@@ -7,80 +7,59 @@ import sys
 from pprint import pprint
 
 
-class Function:
-    def __init__(self):
-        # self.line = -1
-        self.fn = ""
-        self.fl = ""
-
-
 class PHPProfilerParser:
     def __init__(self):
-        self.file = None
+        self.fnRegex = re.compile("fn=\((?P<num>\d+)\)( (php::)?(?P<fn>.+))?")
+        self.flRegex = re.compile("fl=\((?P<num>\d+)\)( (?P<fl>.+))?")
+        self.fnflRegex = re.compile("f[nl]=f[nl]=\((?P<num>\d+)\)")
 
-        # None before starting
-        # "metadata" while parsing metadata
-        # and
-        # "body" while parsing the actual profiling data
-        self.currentParsingSection = "metadata"
-
-        self.fnRegex = re.compile("^fn=\((?P<num>\d+)\)( (php::)?(?P<fn>.+))?")
-        self.flRegex = re.compile("^fl=\((?P<num>\d+)\)( (?P<fl>.+))?")
-        self.fnflRegex = re.compile("^f[nl]=f[nl]=\((?P<num>\d+)\)")
+        self.context = None
 
         self.files = {}
         self.functions = {}
 
 
     def parse(self, filename):
-        self.file = open(filename)
-        self.currentParsingSection = "metadata"
-        while not self.consume_line():
-            pass
-        self.file.close()
+        for line in open(filename):
+            self.consume_line(line)
 
-
-    def consume_line(self):
-        line = self.file.readline().replace("\x00", "")
-        # metadata contains ":"
-        if self.currentParsingSection == "metadata" and (":" in line or len(line) == 1):
-            # noop for metadata, don't really care about it for our use-case
-            pass
-        else:
-            self.currentParsingSection = "body"
+    def consume_line(self, line):
+        line = line.replace("\x00", "")
 
         try:
             if line == '\n':
                 # blank line
-                self.context = Function()
+                return
 
-            if line.startswith("fn=fl") or line.startswith("fl=fl") or line.startswith('fn=fn') or line.startswith('fl=fn'):
+            firstfour = line[:4]
+
+            if firstfour == "fn=f" or firstfour == "fl=f":
                 fnfl = self.fnflRegex.match(line)
                 num = fnfl.group('num')
-                self.context.fl = num
+                self.context = num
 
-                return False
+                return
 
-            if line.startswith("fl"):
+            if firstfour == "fl=(":
                 fl = self.flRegex.match(line)
                 filename = fl.group('fl')
                 num = fl.group('num')
                 if filename:
                     self.files[num] = filename
-                self.context.fl = num
+                self.context = num
 
-            if line.startswith("fn"):
+                return
+
+            if firstfour == "fn=(":
                 fn = self.fnRegex.match(line)
                 func = fn.group('fn')
                 num = fn.group('num')
                 if func:
-                    self.functions[num] = (func, self.files[self.context.fl])
+                    self.functions[num] = (func, self.files[self.context])
         except Exception as e:
             print("Something went wrong while parsing the following line:")
             print(line)
             raise e
-
-        return "{main}" in line or not line
 
 
 def get_function_file_mapping(filenames):
@@ -95,7 +74,7 @@ def get_function_file_mapping(filenames):
         "include": "php::internal",
         "include_once": "php::internal"
     }
-    for k, (func, file) in parser.functions.items():
+    for (func, file) in parser.functions.values():
         mapping[func] = file
 
     return mapping
